416059f5811af05ec4bb46f0f4ca0f46ef099c82
Wilson Ndirangu <wnwillyndirangu@gmail.com>
2022-01-05 08:32:12 +0300

    added tests

---
+++ LR/tests.py
@@ -0,0 +1,14 @@
+from unittest import Testcase
+
+from LR import sigmoid
+from utils import build_features,process_tweet
+
+# tests for the functions
+
+def test_build_features(build_features):
+    pass
+def test_process_tweet(process_tweet):
+    pass
+
+def test_sigmoid(sigmoid):
+    pass
