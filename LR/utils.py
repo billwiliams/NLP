1a9c95dddea3db815604ca115ca84cffd5e818d7
Wilson Ndirangu <wnwillyndirangu@gmail.com>
2022-01-10 08:13:42 +0300

    add prepare data function to utils

--- LR/utils.py
+++ LR/utils.py
@@ -124,5 +124,34 @@ def build_features(tweets,sentiments):
 print(build_features(tweet,[1]))
 
 
+def prepare_data():
+    """
+    prepares the training and testing data from the twitter samples data
+    """
+    # select the set of positive and negative tweets
+    all_positive_tweets = twitter_samples.strings('positive_tweets.json')
+    all_negative_tweets = twitter_samples.strings('negative_tweets.json')
+    # split the data into two pieces, one for training and one for testing (validation set) 
+    test_pos = all_positive_tweets[4000:]
+    train_pos = all_positive_tweets[:4000]
+    test_neg = all_negative_tweets[4000:]
+    train_neg = all_negative_tweets[:4000]
+
+    train_x = train_pos + train_neg 
+    test_x = test_pos + test_neg
+
+    train_y=np.append(np.ones((len(train_pos),1)),np.zeros((len(train_neg),1)),axis=0)
+    test_y=np.append(np.ones((len(test_pos),1)),np.zeros((len(test_neg),1)),axis=0)
+    
+    # check the sizes of the training and testing data
+    print("training data shape is {}",train_y.shape)
+    print("training data shape is {}",test_y.shape)
+   
+
+    return train_x,test_x, train_y,test_y
+
+print(prepare_data())
+
+
 
    
