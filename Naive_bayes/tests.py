6920b6e0f6f0a118cabc81cfec4d7c04d7d3437e
Wilson Ndirangu <wnwillyndirangu@gmail.com>
2022-01-17 08:04:47 +0300

    added more tests on naive bayes

--- Naive_bayes/tests.py
+++ Naive_bayes/tests.py
@@ -1,4 +1,5 @@
 import unittest
+import numpy as np
 from naive_bayes import naive_bayes_predict,naive_bayes,predict_on_test_set
 
 class TestNaiveBayes(unittest.TestCase):
@@ -10,18 +11,19 @@ class TestNaiveBayes(unittest.TestCase):
         self.freqs={('am',1):1,('am',0):1,(':(',0):1,(':(',1):0,('happi',1):1,('happi',0):0}
         self.train_x=["am done :(","am happi"]
         self.train_y=[0,1]
-        self.test_y=[0]
+        self.test_y=np.matrix([0])
+        self.logprior,self.loglikelihood=0.0, {'am': 0.0, ':(': -0.6931471805599453, 'happi': 0.6931471805599453}
         
 
     def test_naive_bayes(self):
-        self.assertEquals( naive_bayes(self.freqs,self.train_x,self.train_y),(0.0, {'am': 0.0, ':(': -0.6931471805599453, 'happi': 0.6931471805599453}))
+        self.assertEqual( naive_bayes(self.freqs,self.train_x,self.train_y),(0.0, {'am': 0.0, ':(': -0.6931471805599453, 'happi': 0.6931471805599453}))
     def test_naive_bayes_predict(self):
-        pass
+        self.assertIsInstance(naive_bayes_predict("am done",self.logprior,self.loglikelihood),float)
     def test_predict_on_test_set(self):
-        logprior,loglikelihood=0.0, {'am': 0.0, ':(': -0.6931471805599453, 'happi': 0.6931471805599453}
-
-        #self.assertEqual(predict_on_test_set(self.test_x,self.test_y,logprior,loglikelihood),100)
         
+
+        # self.assertEqual(predict_on_test_set(self.test_x,self.test_y.flatten(),self.logprior,self.loglikelihood),100)
+        pass
 
 if __name__ == '__main__':
     unittest.main()
