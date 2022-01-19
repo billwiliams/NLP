c7cfd1bac8171b8f8c962c1e68640e2b3609dc64
Wilson Ndirangu <wnwillyndirangu@gmail.com>
2022-01-19 08:24:15 +0300

    added tests to word vectors

---
+++ word_vectors/tests.py
@@ -0,0 +1,12 @@
+import unittest
+
+
+class TestWordVectors(unittest.TestCase):
+    """word vector tests
+
+    """
+    def __init__(self, methodName: str = ...) -> None:
+        super().__init__(methodName)
+    
+if __name__=='__main__':
+    unittest.main()
