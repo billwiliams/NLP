c664ddf88b3eb743c13586329c6d03ed27fa7f37
Wilson Ndirangu <wnwillyndirangu@gmail.com>
2022-01-20 08:32:45 +0300

    add more definitions to PCA

--- word_vectors/PCA.py
+++ word_vectors/PCA.py
@@ -1,4 +1,6 @@
 import numpy as np
+import matplotlib.pyplot as plt
+import sklearn as sklearn
 
 # Implementation of PCA algorithm for dimensionality reduction to enable 
 # view the word vectors relationships from the document corpus
