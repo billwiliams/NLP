58de19457fb80d53d3615b9f2a57c41aae09d51b
Wilson Ndirangu <wnwillyndirangu@gmail.com>
2022-01-24 08:57:04 +0300

    add cosine similarity tests

--- word_vectors/tests.py
+++ word_vectors/tests.py
@@ -1,4 +1,6 @@
 import unittest
+import numpy as np
+from word_vectors import cosine_similarity, accuracy
 
 
 class TestWordVectors(unittest.TestCase):
@@ -7,6 +9,13 @@ class TestWordVectors(unittest.TestCase):
     """
     def __init__(self, methodName: str = ...) -> None:
         super().__init__(methodName)
+    
+    def test_cosine_similarity(self):
+        vector=np.random.rand(3,3)
+        print(cosine_similarity(vector,vector))
+        # self.assertEqual(cosine_similarity(vector,vector),1.0)
+        # self.assertEqual(cosine_similarity(vector,vector),0.0)
+
     
 if __name__=='__main__':
     unittest.main()
