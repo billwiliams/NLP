dfd0c6e497b6ed2889652f70b3a4869393939b3d
Wilson Ndirangu <wnwillyndirangu@gmail.com>
2022-01-27 08:11:29 +0300

    add function to obtain english and corresponding french embedding matrices

--- machine_translation/utils.py
+++ machine_translation/utils.py
@@ -1,3 +1,4 @@
+import numpy as np
 
 def load_dict(file):
     _dict={}
@@ -6,3 +7,31 @@ def load_dict(file):
             (key,val)=line.split()
             _dict[key]=val
     return _dict
+
+def cosine_similarity(A,B):
+    """ computes the cosine similarity of two vectors 
+
+    Args:
+        A : Vector A corresponding to the first word
+        B : Vector corresponding to word B
+    Output:
+        cos: numerical number representing the cosine similarity between A and B.
+    """
+
+    dot = np.dot(A,B)   
+    norma = np.linalg.norm(A)
+    normb = np.linalg.norm(B)    
+    cos = dot/(norma*normb)
+
+    
+    return cos
+
+def process_tweet(tweet):
+    """
+    Remove twitter handles, urls, stopwords
+    Tokenize the string
+    Perform stemming on the word
+
+    """
+    pass
+    
