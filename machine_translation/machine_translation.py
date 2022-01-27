08de2cd19216d1505d22ab2532be73a6b5012bc7
Wilson Ndirangu <wnwillyndirangu@gmail.com>
2022-01-27 20:13:23 +0300

    added test vocabulary fucntion to compute accuracy on the test set

--- machine_translation/machine_translation.py
+++ machine_translation/machine_translation.py
@@ -166,6 +166,49 @@ def align_embeddings(X, Y, train_steps=100, learning_rate=0.0003, verbose=True, 
     
     return R
 
+def test_vocabulary(X, Y, R, nearest_neighbor=nearest_neighbor):
+    '''
+    Input:
+        X: a matrix where the columns are the English embeddings.
+        Y: a matrix where the columns correspong to the French embeddings.
+        R: the transform matrix which translates word embeddings from
+        English to French word vector space.
+    Output:
+        accuracy: for the English to French capitals
+    '''
+
+    # The prediction is X times R
+    # ALTERNATIVE SOLUTION COMMENT:
+    # pred = None
+    pred = np.matmul(X,R)
+
+    # initialize the number correct to zero
+    num_correct = 0
+
+    # loop through each row in pred (each transformed embedding)
+    for i in range(len(pred)):
+        # get the index of the nearest neighbor of pred at row 'i'; also pass in the candidates in Y
+        pred_idx = nearest_neighbor(pred[i],Y)
+
+        # if the index of the nearest neighbor equals the row of i... \
+        if pred_idx == i:
+            # increment the number correct by 1.
+            num_correct += 1
+
+    # accuracy is the number correct divided by the number of rows in 'pred' (also number of rows in X)
+    accuracy = num_correct/len(pred)
+
+
+    return accuracy
+
 # getting the training set:
 X_train, Y_train = get_matrices(
     en_fr_train, fr_embeddings_subset, en_embeddings_subset)
+
+# finding R transformation matrix using align embeddings fucntion to minimize loss
+R_train = align_embeddings(X_train, Y_train, train_steps=400, learning_rate=0.8)
+
+X_val, Y_val = get_matrices(en_fr_test, fr_embeddings_subset, en_embeddings_subset)
+
+acc = test_vocabulary(X_val, Y_val, R_train)  # this might take a minute or two
+print(f"accuracy on test set is {acc:.3f}")
