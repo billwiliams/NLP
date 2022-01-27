034cb5c2881726c8084b610f7d6a949195b97101
Wilson Ndirangu <wnwillyndirangu@gmail.com>
2022-01-27 08:20:48 +0300

    add compute loss function

--- machine_translation/machine_translation.py
+++ machine_translation/machine_translation.py
@@ -90,3 +90,34 @@ def get_matrices(en_fr, french_vecs, english_vecs):
     Y = np.stack(Y_l)
 
     return X, Y
+
+def compute_loss(X, Y, R):
+    '''
+    Inputs: 
+        X: a matrix of dimension (m,n) where the columns are the English embeddings.
+        Y: a matrix of dimension (m,n) where the columns correspong to the French embeddings.
+        R: a matrix of dimension (n,n) - transformation matrix from English to French vector space embeddings.
+    Outputs:
+        L: a matrix of dimension (m,n) - the value of the loss function for given X, Y and R.
+    '''
+    
+    # m is the number of rows in X
+    m = len(X)
+        
+    # diff is XR - Y    
+    diff = np.matmul(X,R) -Y
+
+    # diff_squared is the element-wise square of the difference    
+    diff_squared = np.square(diff)
+
+    # sum_diff_squared is the sum of the squared elements
+    sum_diff_squared = np.sum(diff_squared)
+
+    # loss i the sum_diff_squard divided by the number of examples (m)
+    loss = sum_diff_squared/m
+    
+    return loss
+
+# getting the training set:
+X_train, Y_train = get_matrices(
+    en_fr_train, fr_embeddings_subset, en_embeddings_subset)
