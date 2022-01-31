fe3a4f8e94b08466e026e824a3ff7ee808ba2535
Wilson Ndirangu <wnwillyndirangu@gmail.com>
2022-01-31 08:00:48 +0300

    add tests

---
+++ autocorrect/tests.py
@@ -0,0 +1,9 @@
+import unittest
+
+class TestEditMethod(unittest.TestCase):
+    """ Test edit words methods
+
+    """
+    def __init__(self, methodName: str = ...) -> None:
+        super().__init__(methodName)
+        
