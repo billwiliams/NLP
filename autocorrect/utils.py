a3b098271ed41d00d84b157352cdd1f274e04aee
Wilson Ndirangu <wnwillyndirangu@gmail.com>
2022-02-01 20:02:35 +0300

    add function to get vocanulary from reading a file

--- autocorrect/utils.py
+++ autocorrect/utils.py
@@ -1,1 +1,19 @@
 import numpy as np
+
+def get_vocab(file_name):
+    """get the vocabulary by reading a file
+
+    Args:
+        file_name : path of the file to be read
+    """
+    vocab=[]
+
+    with open(file_name,'r') as file:
+        lines=file.read()
+        for line in lines:
+            line_lower_case=line.lower()
+            for word in line_lower_case.split():
+                vocab.append(word.lower())
+    
+    return vocab
+    
