f6b26dc56b620fdf3d76451efa22c1dacbec0f6a
Wilson Ndirangu <wnwillyndirangu@gmail.com>
2022-02-03 20:17:03 +0300

    add replace_letter function

--- autocorrect/string_manipulations.py
+++ autocorrect/string_manipulations.py
@@ -15,11 +15,11 @@ class StringManipulation:
         delete_l = []
         split_l = []
         
-        ### START CODE HERE ###
+      
         split_l=[(self.word[:i],self.word[i:]) for i in range(len(self.word)+1)]
         delete_l=[L + R[1:] for L, R in split_l if R]
         
-        ### END CODE HERE ###
+        
 
         if verbose: print(f"input word {self.word}, \nsplit_l = {split_l}, \ndelete_l = {delete_l}")
 
@@ -35,16 +35,49 @@ class StringManipulation:
         switch_l = []
         split_l = []
         
-        ### START CODE HERE ###
+        
         split_l=[(self.word[:i],self.word[i:]) for i in range(len(self.word)+1)]
         switch_l=list(set([L+R[:i] + R[i+1] + R[i] + R[i+2:]for (L,R) in split_l for i in range(len(R)-1) if R]))
         
-        ### END CODE HERE ###
+        
         
         if verbose: print(f"Input word = {self.word} \nsplit_l = {split_l} \nswitch_l = {switch_l}") 
         
         return switch_l
+    
+    def replace_letter(self, verbose=False):
+        '''
+        
+        Output:
+            replaces: a list of all possible strings where we replaced one letter from the original word. 
+        ''' 
+        
+        letters = 'abcdefghijklmnopqrstuvwxyz'
+        
+        replace_l = []
+        split_l = []
+        
+        
+        split_l=[(self.word[:i],self.word[i:]) for i in range(len(self.word)+1)]
+        for i in range(len(self.word)):
+            for c in letters:
+                new_word=self.word[:i]+c+self.word[i+1:]
+                if new_word!=self.word:
+                    replace_l.append(new_word)
+    #     replace_l=[c+R for L, R in split_l for c in letters if  len(R)>1 and len(R)<len(word)]
+        replace_set=set(replace_l)
+        
+        
+
+        
+        # turn the set back into a list and sort it, for easier viewing
+        replace_l = sorted(list(replace_set))
+        
+        if verbose: print(f"Input word = {self.word} \nsplit_l = {split_l} \nreplace_l {replace_l}")   
+        
+        return replace_l
 
 SM=StringManipulation('ate')
 print(SM.delete_letter())
 print(SM.switch_letter())
+print(SM.replace_letter())
