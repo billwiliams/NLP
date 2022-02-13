e672de547e632211d46ad893f210bf94d2713d03
Wilson Ndirangu <wnwillyndirangu@gmail.com>
2022-02-13 19:36:36 +0300

    add imports for data collection

---
+++ parts_of_speech/parts_of_speech.py
@@ -0,0 +1,6 @@
+# Importing packages and loading in the data set 
+from utils_pos import get_word_tag, preprocess  
+import pandas as pd
+from collections import defaultdict
+import math
+import numpy as np
