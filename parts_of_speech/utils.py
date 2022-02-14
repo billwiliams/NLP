f4e7b884bb7284d4761cb8728d364b70f37f3b71
Wilson Ndirangu <wnwillyndirangu@gmail.com>
2022-02-14 19:43:06 +0300

    add utils and preprocess fnctn

---
+++ parts_of_speech/utils.py
@@ -0,0 +1,34 @@
+def preprocess(vocab, data_fp):
+    """
+    Preprocess data
+    """
+    orig = []
+    prep = []
+
+    # Read data
+    with open(data_fp, "r") as data_file:
+
+        for cnt, word in enumerate(data_file):
+
+            # End of sentence
+            if not word.split():
+                orig.append(word.strip())
+                word = "--n--"
+                prep.append(word)
+                continue
+
+            # Handle unknown words
+            elif word.strip() not in vocab:
+                orig.append(word.strip())
+                word = assign_unk(word)
+                prep.append(word)
+                continue
+
+            else:
+                orig.append(word.strip())
+                prep.append(word.strip())
+
+    assert(len(orig) == len(open(data_fp, "r").readlines()))
+    assert(len(prep) == len(open(data_fp, "r").readlines()))
+
+    return orig, prep
