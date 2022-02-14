bd4c0a892643e6760099bc16ab1fb0e6a2637dad
Wilson Ndirangu <wnwillyndirangu@gmail.com>
2022-02-14 19:47:52 +0300

    load vocab and trainig data

--- parts_of_speech/parts_of_speech.py
+++ parts_of_speech/parts_of_speech.py
@@ -1,6 +1,24 @@
 # Importing packages and loading in the data set 
-from utils_pos import get_word_tag, preprocess  
+from utils import get_word_tag, preprocess  
 import pandas as pd
 from collections import defaultdict
 import math
 import numpy as np
+
+# get training corpus
+
+with open("../data/pos/WSJ_02-21.pos") as f:
+    training_corpus=f.readlines()
+
+# read the vocabulary data, split by each line of text, and save the list
+with open("./data/hmm_vocab.txt", 'r') as f:
+    voc_l = f.read().split('\n')
+
+# vocab: dictionary that has the index of the corresponding words
+vocab = {}
+
+# Get the index of the corresponding words. 
+for i, word in enumerate(sorted(voc_l)): 
+    vocab[word] = i   
+
+
