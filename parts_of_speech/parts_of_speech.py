3e580fe8c0faf0505517d3ba04dd6b41b3dd659e
Wilson Ndirangu <wnwillyndirangu@gmail.com>
2022-02-15 19:45:16 +0300

    create emmission tag and transition counts dictionaries

--- parts_of_speech/parts_of_speech.py
+++ parts_of_speech/parts_of_speech.py
@@ -11,7 +11,7 @@ with open("../data/pos/WSJ_02-21.pos") as f:
     training_corpus=f.readlines()
 
 # read the vocabulary data, split by each line of text, and save the list
-with open("./data/hmm_vocab.txt", 'r') as f:
+with open("../data/hmm_vocab.txt", 'r') as f:
     voc_l = f.read().split('\n')
 
 # vocab: dictionary that has the index of the corresponding words
@@ -23,8 +23,61 @@ for i, word in enumerate(sorted(voc_l)):
 
 
 # load in the test corpus
-with open("./data/pos/WSJ_24.pos", 'r') as f:
+with open("../data/pos/WSJ_24.pos", 'r') as f:
     y = f.readlines()
 
 #corpus without tags, preprocessed
-_, prep = preprocess(vocab, "./data/pos/test.words")   
+_, prep = preprocess(vocab, "../data/pos/test.words")  
+
+def create_dictionaries(training_corpus, vocab, verbose=True):
+    """
+    Input: 
+        training_corpus: a corpus where each line has a word followed by its tag.
+        vocab: a dictionary where keys are words in vocabulary and value is an index
+    Output: 
+        emission_counts: a dictionary where the keys are (tag, word) and the values are the counts
+        transition_counts: a dictionary where the keys are (prev_tag, tag) and the values are the counts
+        tag_counts: a dictionary where the keys are the tags and the values are the counts
+    """
+    
+    # initialize the dictionaries using defaultdict
+    emission_counts = defaultdict(int)
+    transition_counts = defaultdict(int)
+    tag_counts = defaultdict(int)
+    
+    # Initialize "prev_tag" (previous tag) with the start state, denoted by '--s--'
+    prev_tag = '--s--' 
+    
+    # use 'i' to track the line number in the corpus
+    i = 0 
+    
+    # Each item in the training corpus contains a word and its POS tag
+    # Go through each word and its tag in the training corpus
+    for word_tag in training_corpus:
+        
+        # Increment the word_tag count
+        i += 1
+        
+        # Every 50,000 words, print the word count
+        if i % 50000 == 0 and verbose:
+            print(f"word count = {i}")
+            
+        
+        # get the word and tag using the get_word_tag helper function (imported from utils_pos.py)
+        word, tag = get_word_tag(word_tag,vocab)
+        
+        # Increment the transition count for the previous word and tag
+        transition_counts[(prev_tag, tag)] += 1
+        
+        # Increment the emission count for the tag and word
+        emission_counts[(tag, word)] += 1
+
+        # Increment the tag count
+        tag_counts[tag] += 1
+
+        # Set the previous tag to this tag (for the next iteration of the loop)
+        prev_tag = tag
+        
+        
+        
+    return emission_counts, transition_counts, tag_counts
