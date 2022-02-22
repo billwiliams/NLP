c9b1d506740de2a30d72ee4021059d9c3e139fd1
Wilson Ndirangu <wnwillyndirangu@gmail.com>
2022-02-22 19:25:38 +0300

    add function to initiliaze best_probs and best_paths for viterbi algorithm

--- parts_of_speech/parts_of_speech.py
+++ parts_of_speech/parts_of_speech.py
@@ -88,6 +88,8 @@ print(f"Number of POS tags (number of 'states'): {len(states)}")
 print("View these POS tags (states)")
 print(states)
 
+emission_counts, transition_counts, tag_counts = create_dictionaries(training_corpus, vocab)
+
 
 def create_transition_matrix(alpha, tag_counts, transition_counts):
     ''' 
@@ -201,3 +203,59 @@ def create_emission_matrix(alpha, tag_counts, emission_counts, vocab):
 
     
     return B
+
+def initialize(states, tag_counts, A, B, corpus, vocab):
+    '''
+    Input: 
+        states: a list of all possible parts-of-speech
+        tag_counts: a dictionary mapping each tag to its respective count
+        A: Transition Matrix of dimension (num_tags, num_tags)
+        B: Emission Matrix of dimension (num_tags, len(vocab))
+        corpus: a sequence of words whose POS is to be identified in a list 
+        vocab: a dictionary where keys are words in vocabulary and value is an index
+    Output:
+        best_probs: matrix of dimension (num_tags, len(corpus)) of floats
+        best_paths: matrix of dimension (num_tags, len(corpus)) of integers
+    '''
+    # Get the total number of unique POS tags
+    num_tags = len(tag_counts)
+    
+    # Initialize best_probs matrix 
+    # POS tags in the rows, number of words in the corpus as the columns
+    best_probs = np.zeros((num_tags, len(corpus)))
+    
+    # Initialize best_paths matrix
+    # POS tags in the rows, number of words in the corpus as columns
+    best_paths = np.zeros((num_tags, len(corpus)), dtype=int)
+    
+    # Define the start token
+    s_idx = states.index("--s--")
+    
+    
+    # Go through each of the POS tags
+    for i in range(num_tags): 
+        
+        # Handle the special case when the transition from start token to POS tag i is zero
+        if A[s_idx,i]==0: 
+            
+            # Initialize best_probs at POS tag 'i', column 0, to negative infinity
+            best_probs[i,0] = float("-inf")
+        
+        # For all other cases when transition from start token to POS tag i is non-zero:
+        else:
+            
+            # Initialize best_probs at POS tag 'i', column 0
+            # Check the formula in the instructions above
+            best_probs[i,0] = np.log(A[s_idx,i])+np.log(B[i,vocab[corpus[0]]])
+            
+    
+    return best_probs, best_paths
+
+alpha = 0.001
+A = create_transition_matrix(alpha, tag_counts, transition_counts)
+
+# creating your emission probability matrix.  
+alpha = 0.001
+B = create_emission_matrix(alpha, tag_counts, emission_counts, list(vocab))
+
+best_probs, best_paths = initialize(states, tag_counts, A, B, prep, vocab)
