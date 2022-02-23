c28ba0abf6cf2c2ced89c65698410e0f1cc591a4
Wilson Ndirangu <wnwillyndirangu@gmail.com>
2022-02-23 20:05:23 +0300

    add viterbi forward function

--- parts_of_speech/parts_of_speech.py
+++ parts_of_speech/parts_of_speech.py
@@ -259,3 +259,72 @@ alpha = 0.001
 B = create_emission_matrix(alpha, tag_counts, emission_counts, list(vocab))
 
 best_probs, best_paths = initialize(states, tag_counts, A, B, prep, vocab)
+
+
+def viterbi_forward(A, B, test_corpus, best_probs, best_paths, vocab, verbose=True):
+    '''
+    Input: 
+        A, B: The transition and emission matrices respectively
+        test_corpus: a list containing a preprocessed corpus
+        best_probs: an initilized matrix of dimension (num_tags, len(corpus))
+        best_paths: an initilized matrix of dimension (num_tags, len(corpus))
+        vocab: a dictionary where keys are words in vocabulary and value is an index 
+    Output: 
+        best_probs: a completed matrix of dimension (num_tags, len(corpus))
+        best_paths: a completed matrix of dimension (num_tags, len(corpus))
+    '''
+    # Get the number of unique POS tags (which is the num of rows in best_probs)
+    num_tags = best_probs.shape[0]
+    
+    # Go through every word in the corpus starting from word 1
+    # Recall that word 0 was initialized in `initialize()`
+    for i in range(1, len(test_corpus)): 
+        
+        # Print number of words processed, every 5000 words
+        if i % 5000 == 0 and verbose:
+            print("Words processed: {:>8}".format(i))
+            
+        
+        # For each unique POS tag that the current word can be
+        for j in range(num_tags): # Replace None in this line with the proper range. # for every pos tag
+            
+            # Initialize best_prob for word i to negative infinity
+            best_prob_i = float("-inf")
+            
+            # Initialize best_path for current word i to None
+            best_path_i = None 
+
+            # For each POS tag that the previous word can be:
+            for k in range(num_tags): 
+
+                # Calculate the probability = None
+                # best probs of POS tag k, previous word i-1 + 
+                # log(prob of transition from POS k to POS j) + 
+                # log(prob that emission of POS j is word i)
+                prob = best_probs[k,i-1] +np.log(A[k,j])+np.log(B[j,vocab[test_corpus[i]]])
+
+                # check if this path's probability is greater than
+                # the best probability up to and before this point
+                if prob>best_prob_i: 
+                    
+                    # Keep track of the best probability
+                    best_prob_i = prob
+                    
+                    # keep track of the POS tag of the previous word
+                    # that is part of the best path.  
+                    # Save the index (integer) associated with 
+                    # that previous word's POS tag
+                    best_path_i = k
+
+            # Save the best probability for the 
+            # given current word's POS tag
+            # and the position of the current word inside the corpus
+            best_probs[j,i] = best_prob_i
+            
+            # Save the unique integer ID of the previous POS tag
+            # into best_paths matrix, for the POS tag of the current word
+            # and the position of the current word inside the corpus.
+            best_paths[j,i] = best_path_i
+
+    
+    return best_probs, best_paths
