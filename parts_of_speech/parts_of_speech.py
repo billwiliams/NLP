b915b656d490b6fa4790a988ebbb83296c9f0890
Wilson Ndirangu <wnwillyndirangu@gmail.com>
2022-03-01 19:37:14 +0300

    add compute accuracy function

--- parts_of_speech/parts_of_speech.py
+++ parts_of_speech/parts_of_speech.py
@@ -410,3 +410,42 @@ m=len(pred)
 print('The third word is:', prep[3])
 print('Your prediction is:', pred[3])
 print('Your corresponding label y is: ', y[3])
+
+def compute_accuracy(pred, y):
+    '''
+    Input: 
+        pred: a list of the predicted parts-of-speech 
+        y: a list of lines where each word is separated by a '\t' (i.e. word \t tag)
+    Output: 
+        
+    '''
+    num_correct = 0
+    total = 0
+    
+    # Zip together the prediction and the labels
+    for prediction, y in zip(pred, y):
+        
+        # Split the label into the word and the POS tag
+        word_tag_tuple = y.split()
+        
+        # Check that there is actually a word and a tag
+        # no more and no less than 2 items
+        
+        if len(word_tag_tuple) !=2 : 
+            continue
+
+        # store the word and tag separately
+        word, tag = word_tag_tuple
+        
+        # Check if the POS tag label matches the prediction
+        if tag==prediction:
+            
+            # count the number of times that the prediction
+            # and label match
+            num_correct += 1
+            
+        # keep track of the total number of examples (that have valid labels)
+        total += 1
+
+        
+    return num_correct/total
