5f0207bc2ca5e0f7f4343cc1666f6acf1df2a720
Wilson Ndirangu <wnwillyndirangu@gmail.com>
2022-03-04 22:44:54 +0300

    add tokenize sentences function

--- autocomplete/utils.py
+++ autocomplete/utils.py
@@ -36,3 +36,35 @@ def split_to_sentences(data):
     sentences = [s for s in sentences if len(s) > 0]
     
     return sentences    
+
+
+def tokenize_sentences(sentences):
+    """
+    Tokenize sentences into tokens (words)
+    
+    Args:
+        sentences: List of strings
+    
+    Returns:
+        List of lists of tokens
+    """
+    
+    # Initialize the list of lists of tokenized sentences
+    tokenized_sentences = []
+   
+    
+    # Go through each sentence
+    for sentence in sentences: # complete this line
+        
+        # Convert to lowercase letters
+        sentence = sentence.lower()
+        
+        # Convert into a list of words
+        tokenized = nltk.word_tokenize(sentence)
+        
+        # append the list of words to the list of lists
+        tokenized_sentences.append(tokenized)
+    
+    
+    
+    return tokenized_sentences
