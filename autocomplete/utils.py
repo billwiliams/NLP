71bf654c70ce976b9948d181b36d59f7833495f1
Wilson Ndirangu <wnwillyndirangu@gmail.com>
2022-03-04 22:47:18 +0300

    count words function

--- autocomplete/utils.py
+++ autocomplete/utils.py
@@ -90,3 +90,34 @@ def get_tokenized_data(data):
     
     
     return tokenized_sentences
+
+def count_words(tokenized_sentences):
+    """
+    Count the number of word appearence in the tokenized sentences
+    
+    Args:
+        tokenized_sentences: List of lists of strings
+    
+    Returns:
+        dict that maps word (str) to the frequency (int)
+    """
+            
+    word_counts = {}
+
+    
+    # Loop through each sentence
+    for sentence in tokenized_sentences: # complete this line
+        
+        # Go through each token in the sentence
+        for token in sentence: # complete this line
+
+            # If the token is not in the dictionary yet, set the count to 1
+            if token not in word_counts: # complete this line with the proper condition
+                word_counts[token] = 1
+            
+            # If the token is already in the dictionary, increment the count by 1
+            else:
+                word_counts[token] += 1
+
+    
+    return word_counts
