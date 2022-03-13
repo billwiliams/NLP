7d2d7f0b659e13104b1203594ba62bf8d5645d56
Wilson Ndirangu <wnwillyndirangu@gmail.com>
2022-03-13 19:52:37 +0300

    make counbt matrix function addd

--- autocomplete/autocomplete.py
+++ autocomplete/autocomplete.py
@@ -155,3 +155,35 @@ def estimate_probabilities(previous_n_gram, n_gram_counts, n_plus1_gram_counts, 
         probabilities[word] = probability
 
     return probabilities
+
+def make_count_matrix(n_plus1_gram_counts, vocabulary):
+    # add <e> <unk> to the vocabulary
+    # <s> is omitted since it should not appear as the next word
+    vocabulary = vocabulary + ["<e>", "<unk>"]
+    
+    # obtain unique n-grams
+    n_grams = []
+    for n_plus1_gram in n_plus1_gram_counts.keys():
+        n_gram = n_plus1_gram[0:-1]        
+        n_grams.append(n_gram)
+    n_grams = list(set(n_grams))
+    
+    # mapping from n-gram to row
+    row_index = {n_gram:i for i, n_gram in enumerate(n_grams)}    
+    # mapping from next word to column
+    col_index = {word:j for j, word in enumerate(vocabulary)}    
+    
+    nrow = len(n_grams)
+    ncol = len(vocabulary)
+    count_matrix = np.zeros((nrow, ncol))
+    for n_plus1_gram, count in n_plus1_gram_counts.items():
+        n_gram = n_plus1_gram[0:-1]
+        word = n_plus1_gram[-1]
+        if word not in vocabulary:
+            continue
+        i = row_index[n_gram]
+        j = col_index[word]
+        count_matrix[i, j] = count
+    
+    count_matrix = pd.DataFrame(count_matrix, index=n_grams, columns=vocabulary)
+    return count_matrix
