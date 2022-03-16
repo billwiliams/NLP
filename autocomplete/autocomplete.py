37ea0462f49135edf1d808e9d0d4d69e7daf4b81
Wilson Ndirangu <wnwillyndirangu@gmail.com>
2022-03-16 20:59:31 +0300

    add get many suggestions

--- autocomplete/autocomplete.py
+++ autocomplete/autocomplete.py
@@ -316,3 +316,16 @@ def suggest_a_word(previous_tokens, n_gram_counts, n_plus1_gram_counts, vocabula
    
     
     return suggestion, max_prob
+
+def get_suggestions(previous_tokens, n_gram_counts_list, vocabulary, k=1.0, start_with=None):
+    model_counts = len(n_gram_counts_list)
+    suggestions = []
+    for i in range(model_counts-1):
+        n_gram_counts = n_gram_counts_list[i]
+        n_plus1_gram_counts = n_gram_counts_list[i+1]
+        
+        suggestion = suggest_a_word(previous_tokens, n_gram_counts,
+                                    n_plus1_gram_counts, vocabulary,
+                                    k=k, start_with=start_with)
+        suggestions.append(suggestion)
+    return suggestions
