0c6a568c986fdf64f1b401d4c14dc4328768787a
Wilson Ndirangu <wnwillyndirangu@gmail.com>
2022-04-05 20:18:56 +0300

    get index function

--- word_embeddings/utils.py
+++ word_embeddings/utils.py
@@ -6,3 +6,11 @@ from collections import defaultdict
 def sigmoid(z):
     # sigmoid function
     return 1.0 / (1.0 + np.exp(-z))
+
+
+def get_idx(words, word2Ind):
+    idx = []
+    for word in words:
+        idx = idx + [word2Ind[word]]
+    return idx
+    
