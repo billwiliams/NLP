9558e5f4ce7e1acde3412d5e57c52d8e08883cbe
Wilson Ndirangu <wnwillyndirangu@gmail.com>
2022-04-05 20:19:27 +0300

    pack index with frequency

--- word_embeddings/utils.py
+++ word_embeddings/utils.py
@@ -13,4 +13,15 @@ def get_idx(words, word2Ind):
     for word in words:
         idx = idx + [word2Ind[word]]
     return idx
-    
+
+def pack_idx_with_frequency(context_words, word2Ind):
+    freq_dict = defaultdict(int)
+    for word in context_words:
+        freq_dict[word] += 1
+    idxs = get_idx(context_words, word2Ind)
+    packed = []
+    for i in range(len(idxs)):
+        idx = idxs[i]
+        freq = freq_dict[context_words[i]]
+        packed.append((idx, freq))
+    return packed
