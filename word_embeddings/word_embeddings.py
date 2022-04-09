2ccd9580c13bb77c9a025a5816d665926f25eb92
Wilson Ndirangu <wnwillyndirangu@gmail.com>
2022-04-09 16:59:12 +0300

    forward propagation

--- word_embeddings/word_embeddings.py
+++ word_embeddings/word_embeddings.py
@@ -65,3 +65,27 @@ def softmax(z):
     yhat = (np.exp(z))/np.sum(np.exp(z),axis=0)
     
     return yhat
+
+def forward_prop(x, W1, W2, b1, b2):
+    '''
+    Inputs: 
+        x:  average one hot vector for the context 
+        W1, W2, b1, b2:  matrices and biases to be learned
+     Outputs: 
+        z:  output score vector
+    '''
+    
+   
+    # Calculate h
+    h = np.dot(W1,x) + b1
+  
+    # Apply the relu on h, 
+    # store the relu in h
+    h[h < 0] = 0
+
+    # Calculate z
+    z = np.dot(W2,h) + b2
+
+   
+
+    return z, h
