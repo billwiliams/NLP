31661b275e7f2f7581a8cbd8b8cd1358dda61905
Wilson Ndirangu <wnwillyndirangu@gmail.com>
2022-04-10 18:27:14 +0300

    gradient descent

--- word_embeddings/word_embeddings.py
+++ word_embeddings/word_embeddings.py
@@ -133,3 +133,68 @@ def back_prop(x, yhat, y, h, W1, W2, b1, b2, batch_size):
     
     
     return grad_W1, grad_W2, grad_b1, grad_b2
+
+
+def gradient_descent(data, word2Ind, N, V, num_iters, alpha=0.03, 
+                     random_seed=282, initialize_model=initialize_model, 
+                     get_batches=get_batches, forward_prop=forward_prop, 
+                     softmax=softmax, compute_cost=compute_cost, 
+                     back_prop=back_prop):
+    
+    '''
+    This is the gradient_descent function
+    
+      Inputs: 
+        data:      text
+        word2Ind:  words to Indices
+        N:         dimension of hidden vector  
+        V:         dimension of vocabulary 
+        num_iters: number of iterations  
+        random_seed: random seed to initialize the model's matrices and vectors
+        initialize_model: your implementation of the function to initialize the model
+        get_batches: function to get the data in batches
+        forward_prop: your implementation of the function to perform forward propagation
+        softmax: your implementation of the softmax function
+        compute_cost: cost function (Cross entropy)
+        back_prop: your implementation of the function to perform backward propagation
+     Outputs: 
+        W1, W2, b1, b2:  updated matrices and biases after num_iters iterations
+
+    '''
+    W1, W2, b1, b2 = initialize_model(N,V, random_seed=random_seed) #W1=(N,V) and W2=(V,N)
+
+    batch_size = 128
+#    batch_size = 512
+    iters = 0
+    C = 2 
+    
+    for x, y in get_batches(data, word2Ind, V, C, batch_size):
+                      
+        # get z and h
+        z, h = forward_prop(x, W1, W2, b1, b2)
+                
+        # get yhat
+        yhat = softmax(z)
+        
+        # get cost
+        cost = compute_cost(y, yhat, batch_size)
+        if ( (iters+1) % 10 == 0):
+            print(f"iters: {iters + 1} cost: {cost:.6f}")
+            
+        # get gradients
+        grad_W1, grad_W2, grad_b1, grad_b2 = back_prop(x, yhat, y, h, W1, W2, b1, b2, batch_size)
+        
+        # update weights and biases
+        W1 = W1-alpha*grad_W1
+        W2 = W2-alpha*grad_W2
+        b1 = b1-alpha*grad_b1
+        b2 = b2-alpha*grad_b2
+
+        
+        iters +=1 
+        if iters == num_iters: 
+            break
+        if iters % 100 == 0:
+            alpha *= 0.66
+            
+    return W1, W2, b1, b2
