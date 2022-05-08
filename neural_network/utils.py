9f01b7c747fb7af5f63b7b8fd9d6c925164a134e
Wilson Ndirangu <wnwillyndirangu@gmail.com>
2022-05-08 19:28:18 +0300

    layer class definition

--- neural_network/utils.py
+++ neural_network/utils.py
@@ -63,3 +63,21 @@ def load_tweets():
 
 # Layers have weights and a foward function.
 # They create weights when layer.initialize is called and use them.
+
+class Layer(object):
+    """Base class for layers."""
+    def __init__(self):
+        self.weights = None
+
+    def forward(self, x):
+        raise NotImplementedError
+  
+    def init_weights_and_state(self, input_signature, random_key):
+        pass
+
+    def init(self, input_signature, random_key):
+        self.init_weights_and_state(input_signature, random_key)
+        return self.weights
+    
+    def __call__(self, x):
+        return self.forward(x)
