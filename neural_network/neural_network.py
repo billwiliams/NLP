f44846b15e5633032abca6654ecdde5c1cff8b95
Wilson Ndirangu <wnwillyndirangu@gmail.com>
2022-05-15 19:52:28 +0300

    data generator

--- neural_network/neural_network.py
+++ neural_network/neural_network.py
@@ -117,3 +117,178 @@ def tweet_to_tensor(tweet, vocab_dict, unk_token='__UNK__', verbose=False):
    
 
     return tensor_l
+
+def data_generator(data_pos, data_neg, batch_size, loop, vocab_dict, shuffle=False):
+    '''
+    Input: 
+        data_pos - Set of positive examples
+        data_neg - Set of negative examples
+        batch_size - number of samples per batch. Must be even
+        loop - True or False
+        vocab_dict - The words dictionary
+        shuffle - Shuffle the data order
+    Yield:
+        inputs - Subset of positive and negative examples
+        targets - The corresponding labels for the subset
+        example_weights - An array specifying the importance of each example
+        
+    '''     
+
+    # make sure the batch size is an even number
+    # to allow an equal number of positive and negative samples    
+    assert batch_size % 2 == 0
+    
+    # Number of positive examples in each batch is half of the batch size
+    # same with number of negative examples in each batch
+    n_to_take = batch_size // 2
+    
+    # Use pos_index to walk through the data_pos array
+    # same with neg_index and data_neg
+    pos_index = 0
+    neg_index = 0
+    
+    len_data_pos = len(data_pos)
+    len_data_neg = len(data_neg)
+    
+    # Get and array with the data indexes
+    pos_index_lines = list(range(len_data_pos))
+    neg_index_lines = list(range(len_data_neg))
+    
+    # shuffle lines if shuffle is set to True
+    if shuffle:
+        rnd.shuffle(pos_index_lines)
+        rnd.shuffle(neg_index_lines)
+        
+    stop = False
+    
+    # Loop indefinitely
+    while not stop:  
+        
+        # create a batch with positive and negative examples
+        batch = []
+        
+        # First part: Pack n_to_take positive examples
+        
+        # Start from pos_index and increment i up to n_to_take
+        for i in range(n_to_take):
+                    
+            # If the positive index goes past the positive dataset,
+            if pos_index >= len_data_pos: 
+                
+                # If loop is set to False, break once we reach the end of the dataset
+                if not loop:
+                    stop = True;
+                    break;
+                # If user wants to keep re-using the data, reset the index
+                pos_index = 0
+                if shuffle:
+                    # Shuffle the index of the positive sample
+                    rnd.shuffle(pos_index_lines)
+                    
+            # get the tweet as pos_index
+            tweet = data_pos[pos_index_lines[pos_index]]
+            
+            # convert the tweet into tensors of integers representing the processed words
+            tensor = tweet_to_tensor(tweet, vocab_dict)
+            
+            # append the tensor to the batch list
+            batch.append(tensor)
+            
+            # Increment pos_index by one
+            pos_index = pos_index + 1
+
+
+        
+        # Using the same batch list, start from neg_index and increment i up to n_to_take
+        for i in range(None):
+            
+            # If the negative index goes past the negative dataset,
+            if None
+                
+                # If loop is set to False, break once we reach the end of the dataset
+                if not loop:
+                    stop = True 
+                    break 
+                    
+                # If user wants to keep re-using the data, reset the index
+                neg_index = None
+                
+                if shuffle:
+                    # Shuffle the index of the negative sample
+                    rnd.shuffle(neg_index_lines)
+                    
+            # get the tweet as neg_index
+            tweet = None
+            
+            # convert the tweet into tensors of integers representing the processed words
+            tensor = None
+            
+            # append the tensor to the batch list
+            None
+            
+            # Increment neg_index by one
+            neg_index = None
+
+              
+
+        if stop:
+            break;
+
+        # Update the start index for positive data 
+        # so that it's n_to_take positions after the current pos_index
+        pos_index += n_to_take
+        
+        # Update the start index for negative data 
+        # so that it's n_to_take positions after the current neg_index
+        neg_index += n_to_take
+        
+        # Get the max tweet length (the length of the longest tweet) 
+        # (you will pad all shorter tweets to have this length)
+        max_len = max([len(t) for t in batch]) 
+        
+        
+        # Initialize the input_l, which will 
+        # store the padded versions of the tensors
+        tensor_pad_l = []
+        # Pad shorter tweets with zeros
+        for tensor in batch:
+
+
+
+            # Get the number of positions to pad for this tensor so that it will be max_len long
+            n_pad = None
+            
+            # Generate a list of zeros, with length n_pad
+            pad_l = None
+            
+            # concatenate the tensor and the list of padded zeros
+            tensor_pad = None
+            
+            # append the padded tensor to the list of padded tensors
+            None
+
+        # convert the list of padded tensors to a numpy array
+        # and store this as the model inputs
+        inputs = None
+  
+        # Generate the list of targets for the positive examples (a list of ones)
+        # The length is the number of positive examples in the batch
+        target_pos = None
+        
+        # Generate the list of targets for the negative examples (a list of zeros)
+        # The length is the number of negative examples in the batch
+        target_neg = None
+        
+        # Concatenate the positve and negative targets
+        target_l = None
+        
+        # Convert the target list into a numpy array
+        targets = None
+
+        # Example weights: Treat all examples equally importantly.
+        example_weights = None
+        
+
+        
+        # note we use yield and not return
+        yield inputs, targets, example_weights
