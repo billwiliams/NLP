283135cf35e3664868e68c838887524f14241be8
Wilson Ndirangu <wnwillyndirangu@gmail.com>
2022-05-24 21:26:30 +0300

    evaluate predictions

--- neural_network/neural_network.py
+++ neural_network/neural_network.py
@@ -513,4 +513,58 @@ tmp_inputs, tmp_targets, tmp_example_weights = tmp_batch
 print(f"The batch is a tuple of length {len(tmp_batch)} because position 0 contains the tweets, and position 1 contains the targets.") 
 print(f"The shape of the tweet tensors is {tmp_inputs.shape} (num of examples, length of tweet tensors)")
 print(f"The shape of the labels is {tmp_targets.shape}, which is the batch size.")
-print(f"The shape of the example_weights is {tmp_example_weights.shape}, which is the same as inputs/targets size."
+print(f"The shape of the example_weights is {tmp_example_weights.shape}, which is the same as inputs/targets size.")
+
+# feed the tweet tensors into the model to get a prediction
+tmp_pred = training_loop.eval_model(tmp_inputs)
+print(f"The prediction shape is {tmp_pred.shape}, num of tensor_tweets as rows")
+print("Column 0 is the probability of a negative sentiment (class 0)")
+print("Column 1 is the probability of a positive sentiment (class 1)")
+print()
+print("View the prediction array")
+
+def compute_accuracy(preds, y, y_weights):
+    """
+    Input: 
+        preds: a tensor of shape (dim_batch, output_dim) 
+        y: a tensor of shape (dim_batch,) with the true labels
+        y_weights: a n.ndarray with the a weight for each example
+    Output: 
+        accuracy: a float between 0-1 
+        weighted_num_correct (np.float32): Sum of the weighted correct predictions
+        sum_weights (np.float32): Sum of the weights
+    """
+    # Create an array of booleans, 
+    # True if the probability of positive sentiment is greater than
+    # the probability of negative sentiment
+    # else False
+    is_pos =[pred[1]>pred[0] for pred in preds]
+
+    # convert the array of booleans into an array of np.int32
+    is_pos_int = np.array(is_pos, dtype=np.int32)
+    
+    # compare the array of predictions (as int32) with the target (labels) of type int32
+    correct = y==is_pos_int
+
+    # Count the sum of the weights.
+    sum_weights = sum(y_weights)
+    
+    # convert the array of correct predictions (boolean) into an arrayof np.float32
+    correct_float = np.array(correct,dtype=np.float32)
+    
+    
+    # Multiply each prediction with its corresponding weight.
+    weighted_correct_float = y_weights*correct_float
+    
+
+    # Sum up the weighted correct predictions (of type np.float32), to go in the
+    # denominator.
+    weighted_num_correct = sum(weighted_correct_float)
+    print()
+    
+    # Divide the number of weighted correct predictions by the sum of the
+    # weights.
+    accuracy = sum_weights/weighted_num_correct
+
+   
+    return accuracy, weighted_num_correct, sum_weights
