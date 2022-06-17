050535ac0e1fd80c9e74ed0bfe34fa43a74e8bed
Wilson Ndirangu <wnwillyndirangu@gmail.com>
2022-06-17 21:13:34 +0300

    line to tensor'

--- deep_n_grams/deep_n_grams.py
+++ deep_n_grams/deep_n_grams.py
@@ -39,3 +39,34 @@ lines = lines[:-1000] # Leave the rest for training
 
 print(f"Number of lines for training: {len(lines)}")
 print(f"Number of lines for validation: {len(eval_lines)}")
+
+def line_to_tensor(line, EOS_int=1):
+    """Turns a line of text into a tensor
+
+    Args:
+        line (str): A single line of text.
+        EOS_int (int, optional): End-of-sentence integer. Defaults to 1.
+
+    Returns:
+        list: a list of integers (unicode values) for the characters in the `line`.
+    """
+    
+    # Initialize the tensor as an empty list
+    tensor = []
+    
+    
+    # for each character:
+    for c in line:
+        
+        # convert to unicode int
+        c_int = ord(c)
+        
+        # append the unicode integer to the tensor list
+        tensor.append(c_int)
+    
+    # include the end-of-sentence integer
+    tensor.append(EOS_int)
+    
+    
+
+    return tensor
