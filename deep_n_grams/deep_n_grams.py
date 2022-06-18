1dba426ff7822509e24fbf1724a9de5e1089ccad
Wilson Ndirangu <wnwillyndirangu@gmail.com>
2022-06-18 07:41:23 +0300

    add the GRU model

--- deep_n_grams/deep_n_grams.py
+++ deep_n_grams/deep_n_grams.py
@@ -169,8 +169,32 @@ def data_generator(batch_size, max_length, data_lines, line_to_tensor=line_to_te
             cur_batch = []
 
 
-import itertools
+# import itertools
 
-infinite_data_generator = itertools.cycle(
-    data_generator(batch_size=2, max_length=10, data_lines=tmp_lines))
+# infinite_data_generator = itertools.cycle(
+#     data_generator(batch_size=2, max_length=10, data_lines=tmp_lines))
+
+def GRULM(vocab_size=256, d_model=512, n_layers=2, mode='train'):
+    """Returns a GRU language model.
+
+    Args:
+        vocab_size (int, optional): Size of the vocabulary. Defaults to 256.
+        d_model (int, optional): Depth of embedding (n_units in the GRU cell). Defaults to 512.
+        n_layers (int, optional): Number of GRU layers. Defaults to 2.
+        mode (str, optional): 'train', 'eval' or 'predict', predict mode is for fast inference. Defaults to "train".
+
+    Returns:
+        trax.layers.combinators.Serial: A GRU language model as a layer that maps from a tensor of tokens to activations over a vocab set.
+    """
+    
+    model = tl.Serial( 
+      tl.ShiftRight(mode=mode), # Stack the ShiftRight layer
+      tl.Embedding(vocab_size, d_model), # Stack the embedding layer
+      [tl.GRU(d_model) for i in range(n_layers)], # Stack GRU layers of d_model units keeping n_layer parameter in mind (use list comprehension syntax)
+      tl.Dense(vocab_size), # Dense layer
+      tl.LogSoftmax() # Log Softmax
+    ) 
+   
+    return model
+
             
