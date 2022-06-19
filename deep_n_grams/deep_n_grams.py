4e60389b8b1affc5fe0cdeaa6557f254655cd0dc
Wilson Ndirangu <wnwillyndirangu@gmail.com>
2022-06-19 19:46:32 +0300

    training loop for trax model

--- deep_n_grams/deep_n_grams.py
+++ deep_n_grams/deep_n_grams.py
@@ -197,4 +197,58 @@ def GRULM(vocab_size=256, d_model=512, n_layers=2, mode='train'):
    
     return model
 
+from trax.supervised import training
+
+
+def train_model(model, data_generator, lines, eval_lines, batch_size=32, max_length=64, n_steps=1, output_dir='model/'): 
+    """Function that trains the model
+
+    Args:
+        model (trax.layers.combinators.Serial): GRU model.
+        data_generator (function): Data generator function.
+        batch_size (int, optional): Number of lines per batch. Defaults to 32.
+        max_length (int, optional): Maximum length allowed for a line to be processed. Defaults to 64.
+        lines (list): List of lines to use for training. Defaults to lines.
+        eval_lines (list): List of lines to use for evaluation. Defaults to eval_lines.
+        n_steps (int, optional): Number of steps to train. Defaults to 1.
+        output_dir (str, optional): Relative path of directory to save model. Defaults to "model/".
+
+    Returns:
+        trax.supervised.training.Loop: Training loop for the model.
+    """
+    
+    
+    bare_train_generator = data_generator(batch_size=batch_size, max_length=max_length, data_lines=lines,shuffle=True)
+    infinite_train_generator = itertools.cycle(data_generator(batch_size=batch_size, max_length=max_length, data_lines=lines,shuffle=True))
+    
+    bare_eval_generator = data_generator(batch_size=batch_size, max_length=max_length, data_lines=eval_lines,shuffle=True)
+    infinite_eval_generator = itertools.cycle(data_generator(batch_size=batch_size, max_length=max_length, data_lines=eval_lines,shuffle=True))
+    
+    train_task = training.TrainTask( 
+        labeled_data=infinite_train_generator, # Use infinite train data generator
+        loss_layer= tl.CrossEntropyLoss(),   # Don't forget to instantiate this object
+        optimizer=trax.optimizers.Adam(learning_rate=0.0005)      # Don't forget to add the learning rate parameter TO 0.0005
+    ) 
+    
+    eval_task = training.EvalTask( 
+        labeled_data=infinite_eval_generator,    # Use infinite eval data generator
+        metrics=[tl.CrossEntropyLoss(),  tl.Accuracy()], # Don't forget to instantiate these objects
+        n_eval_batches=3  # For better evaluation accuracy in reasonable time 
+    ) 
+    
+    training_loop = training.Loop(model, 
+                                  train_task, 
+                                  eval_tasks=[eval_task], 
+                                  output_dir=output_dir) 
+
+    training_loop.run(n_steps=n_steps)
+    
+    
+    
+    # We return this because it contains a handle to the model, which has the weights etc.
+    return training_loop
+
+
+
+
             
