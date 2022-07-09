79e998cc92b15ffb5b4a8a832ba359ad222b6cba
Wilson Ndirangu <wnwillyndirangu@gmail.com>
2022-07-09 08:27:01 +0300

    NER MODEL

--- named_entity_recognition/ner.py
+++ named_entity_recognition/ner.py
@@ -109,3 +109,23 @@ def data_generator(batch_size, x, y, pad, shuffle=False, verbose=False):
 
         if verbose: print("index=", index)
         yield((X,Y))
+
+
+def NER(tags, vocab_size=35181, d_model=50):
+    '''
+      Input: 
+        tag_map - dictionary that maps the tags to numbers
+        vocab_size - integer containing the size of the vocabulary
+        d_model - integer describing the embedding size
+      Output:
+        model - a trax serial model
+    '''
+   
+    model = tl.Serial( 
+      tl.Embedding(vocab_size=vocab_size,d_feature=d_model), # Embedding layer
+      tl.LSTM(d_model), # LSTM layer
+      tl.Dense(len(tags)), # Dense layer with len(tags) units
+      tl.LogSoftmax() # LogSoftmax layer
+      ) 
+
+    return model
