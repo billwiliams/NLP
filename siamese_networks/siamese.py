c239b66ae319a393fe34bcb21e4953a35666ea3f
Wilson Ndirangu <wnwillyndirangu@gmail.com>
2022-09-05 19:59:12 +0300

    data generator fnction

--- siamese_networks/siamese.py
+++ siamese_networks/siamese.py
@@ -56,33 +56,33 @@ vocab = defaultdict(lambda: 0)
 vocab['<PAD>'] = 1
 
 for idx in range(len(Q1_train_words)):
-        Q1_train[idx] = nltk.word_tokenize(Q1_train_words[idx])
-            Q2_train[idx] = nltk.word_tokenize(Q2_train_words[idx])
-                q = Q1_train[idx] + Q2_train[idx]
-                    for word in q:
-                                if word not in vocab:
-                                                vocab[word] = len(vocab) + 1
-                                                print('The length of the vocabulary is: ', len(vocab))
+    Q1_train[idx] = nltk.word_tokenize(Q1_train_words[idx])
+    Q2_train[idx] = nltk.word_tokenize(Q2_train_words[idx])
+    q = Q1_train[idx] + Q2_train[idx]
+    for word in q:
+        if word not in vocab:
+            vocab[word] = len(vocab) + 1
+            print('The length of the vocabulary is: ', len(vocab))
 
 print(vocab['<PAD>'])
 print(vocab['Astrology'])
 print(vocab['Astronomy'])  #not in vocabulary, returns 0
 
 for idx in range(len(Q1_test_words)): 
-        Q1_test[idx] = nltk.word_tokenize(Q1_test_words[idx])
-            Q2_test[idx] = nltk.word_tokenize(Q2_test_words[idx])
+    Q1_test[idx] = nltk.word_tokenize(Q1_test_words[idx])
+    Q2_test[idx] = nltk.word_tokenize(Q2_test_words[idx])
 
 print('Train set has reduced to: ', len(Q1_train) ) 
 print('Test set length: ', len(Q1_test) ) 
 # Converting questions to array of integers
 for i in range(len(Q1_train)):
-        Q1_train[i] = [vocab[word] for word in Q1_train[i]]
-            Q2_train[i] = [vocab[word] for word in Q2_train[i]]
+    Q1_train[i] = [vocab[word] for word in Q1_train[i]]
+    Q2_train[i] = [vocab[word] for word in Q2_train[i]]
 
                     
-                    for i in range(len(Q1_test)):
-                            Q1_test[i] = [vocab[word] for word in Q1_test[i]]
-                                Q2_test[i] = [vocab[word] for word in Q2_test[i]]
+    for i in range(len(Q1_test)):
+        Q1_test[i] = [vocab[word] for word in Q1_test[i]]
+        Q2_test[i] = [vocab[word] for word in Q2_test[i]]
 
 # Splitting the data
 cut_off = int(len(Q1_train)*.8)
@@ -94,6 +94,74 @@ print("The length of the validation set is: ", len(val_Q1))
 
 
 # create data generator
+
+def data_generator(Q1, Q2, batch_size, pad=1, shuffle=True):
+    """Generator function that yields batches of data
+
+    Args:
+        Q1 (list): List of transformed (to tensor) questions.
+        Q2 (list): List of transformed (to tensor) questions.
+        batch_size (int): Number of elements per batch.
+        pad (int, optional): Pad character from the vocab. Defaults to 1.
+        shuffle (bool, optional): If the batches should be randomnized or not. Defaults to True.
+    Yields:
+        tuple: Of the form (input1, input2) with types (numpy.ndarray, numpy.ndarray)
+        NOTE: input1: inputs to your model [q1a, q2a, q3a, ...] i.e. (q1a,q1b) are duplicates
+              input2: targets to your model [q1b, q2b,q3b, ...] i.e. (q1a,q2i) i!=a are not duplicates
+    """
+
+    input1 = []
+    input2 = []
+    idx = 0
+    len_q = len(Q1)
+    question_indexes = [*range(len_q)]
+    
+    if shuffle:
+        rnd.shuffle(question_indexes)
+    
+    while True:
+        if idx >= len_q:
+            # if idx is greater than or equal to len_q, set idx accordingly 
+            # (Hint: look at the instructions above)
+            idx = 0
+            # shuffle to get random batches if shuffle is set to True
+            if shuffle:
+                rnd.shuffle(question_indexes) 
+        
+        # get questions at the `question_indexes[idx]` position in Q1 and Q2
+        q1 = Q1[question_indexes[idx]]
+        q2 = Q2[question_indexes[idx]]
+        
+        # increment idx by 1
+        idx += 1
+        # append q1
+        input1.append(q1)
+        # append q2
+        input2.append(q2)
+        if len(input1) == batch_size:
+            # determine max_len as the longest question in input1 & input 2
+            # Hint: use the `max` function. 
+            # take max of input1 & input2 and then max out of the two of them.
+            
+            max_len =max(max([len(i) for i in input1]),max([len(i) for i in input2]))
+            # pad to power-of-2 (Hint: look at the instructions above)
+            max_len = 2**int(np.ceil(np.log2(max_len)))
+            b1 = [] 
+            b2 = [] 
+            for q1, q2 in zip(input1, input2):
+                # add [pad] to q1 until it reaches max_len
+                q1 = q1 +[pad]*(max_len-len(q1))
+                # add [pad] to q2 until it reaches max_len
+                q2 = q2 +[pad]*(max_len-len(q2))                
+                # append q1
+                b1.append(q1)
+                # append q2
+                b2.append(q2)
+            # use b1 and b2
+            yield np.array(b1), np.array(b2)
+    
+            # reset the batches
+            input1, input2 = [], []  # reset the batches
 
 batch_size = 2
 res1, res2 = next(data_generator(train_Q1, train_Q2, batch_size))
