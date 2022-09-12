cd68d8478047e08b6b784982003d2801e5b05be7
Wilson Ndirangu <wnwillyndirangu@gmail.com>
2022-09-12 20:15:02 +0300

    create model

--- siamese_networks/siamese.py
+++ siamese_networks/siamese.py
@@ -172,9 +172,40 @@ def TripletLoss(margin=0.25):
         triplet_loss_fn = partial(TripletLossFn, margin=margin)
             return tl.Fn('TripletLoss', triplet_loss_fn)
 
-# Model
+# Model 
 
-# check your model
+def Siamese(vocab_size=41699, d_model=128, mode='train'):
+    """Returns a Siamese model.
+
+    Args:
+        vocab_size (int, optional): Length of the vocabulary. Defaults to len(vocab).
+        d_model (int, optional): Depth of the model. Defaults to 128.
+        mode (str, optional): 'train', 'eval' or 'predict', predict mode is for fast inference. Defaults to 'train'.
+
+    Returns:
+        trax.layers.combinators.Parallel: A Siamese model. 
+    """
+
+    def normalize(x):  # normalizes the vectors to have L2 norm 1
+        return x / fastnp.sqrt(fastnp.sum(x * x, axis=-1, keepdims=True))
+    
+   
+    q_processor = tl.Serial( # Processor will run on Q1 and Q2. 
+        tl.Embedding(vocab_size,d_model), # Embedding layer
+        tl.LSTM(d_model), # LSTM layer
+        tl.Mean(axis=1), # Mean over columns
+        tl.Fn('Normalize', lambda x: normalize(x)), # Apply normalize function
+    )  # Returns one vector of shape [batch_size, d_model]. 
+    
+    
+    # Run on Q1 and Q2 in parallel.
+    model = tl.Parallel(q_processor, q_processor)
+    return model
+
+# check  model
+
+# Model 
+
 model = Siamese()
 print(model)
 
