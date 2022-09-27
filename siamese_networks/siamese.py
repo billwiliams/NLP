3693c6b1e64c3399c238a565cafbdf7ff8a6ec60
Wilson Ndirangu <wnwillyndirangu@gmail.com>
2022-09-27 20:08:32 +0300

    refactor predict

--- siamese_networks/siamese.py
+++ siamese_networks/siamese.py
@@ -169,8 +169,8 @@ print("First questions  : ",'\n', res1, '\n')
 print("Second questions : ",'\n', res2)
 from functools import partial
 def TripletLoss(margin=0.25):
-        triplet_loss_fn = partial(TripletLossFn, margin=margin)
-            return tl.Fn('TripletLoss', triplet_loss_fn)
+    triplet_loss_fn = partial(TripletLossFn, margin=margin)
+    return tl.Fn('TripletLoss', triplet_loss_fn)
 
 # Model 
 
@@ -278,59 +278,59 @@ question2 = "When can I see you again?"
 
 
 #predict
-
+ 
 def predict(question1, question2, threshold, model, vocab, data_generator=data_generator, verbose=False):
-        """Function for predicting if two questions are duplicates.
+    """Function for predicting if two questions are duplicates.
 
-            Args:
-                        question1 (str): First question.
-                                question2 (str): Second question.
-                                        threshold (float): Desired threshold.
-                                                model (trax.layers.combinators.Parallel): The Siamese model.
-                                                        vocab (collections.defaultdict): The vocabulary used.
-                                                                data_generator (function): Data generator function. Defaults to data_generator.
-                                                                        verbose (bool, optional): If the results should be printed out. Defaults to False.
+    Args:
+        question1 (str): First question.
+        question2 (str): Second question.
+        threshold (float): Desired threshold.
+        model (trax.layers.combinators.Parallel): The Siamese model.
+        vocab (collections.defaultdict): The vocabulary used.
+        data_generator (function): Data generator function. Defaults to data_generator.
+        verbose (bool, optional): If the results should be printed out. Defaults to False.
 
-                                                                            Returns:
-                                                                                        bool: True if the questions are duplicates, False otherwise.
-                                                                                            """
-                                                                                                ### START CODE HERE (Replace instances of 'None' with your code) ###
-                                                                                                    # use `nltk` word tokenize function to tokenize
-                                                                                                        q1 = nltk.word_tokenize(question1)  # tokenize
-                                                                                                            q2 = nltk.word_tokenize(question2)  # tokenize
-                                                                                                                Q1, Q2 = [], [] # @KEEPTHIS
-                                                                                                                    for word in q1:  # encode q1
-                                                                                                                            # increment by checking the 'word' index in `vocab`
-                                                                                                                                    Q1 += [vocab[word]]
-                                                                                                                                            # GRADING COMMENT: Also valid to use
-                                                                                                                                                    # Q1.extend([vocab[word]])
-                                                                                                                                                        for word in q2:  # encode q2
-                                                                                                                                                                # increment by checking the 'word' index in `vocab`
-                                                                                                                                                                        Q2 +=  [vocab[word]]
-                                                                                                                                                                                # GRADING COMMENT: Also valid to use
-                                                                                                                                                                                        # Q2.extend([vocab[word]])
-                                                                                                                                                                                            
-                                                                                                                                                                                                # GRADING COMMENT: Q1 and Q2 need to be nested inside another list
-                                                                                                                                                                                                    # Call the data generator (built in Ex 01) using next()
-                                                                                                                                                                                                        # pass [Q1] & [Q2] as Q1 & Q2 arguments of the data generator. Set batch size as 1
-                                                                                                                                                                                                            # Hint: use `vocab['<PAD>']` for the `pad` argument of the data generator
-                                                                                                                                                                                                                Q1, Q2 = next(data_generator([Q1], [Q2], 1, pad=vocab['<PAD>'], shuffle=False))
-                                                                                                                                                                                                                    # Call the model
-                                                                                                                                                                                                                        v1, v2 = model((Q1,Q2))
-                                                                                                                                                                                                                            # take dot product to compute cos similarity of each pair of entries, v1, v2
-                                                                                                                                                                                                                                # don't forget to transpose the second argument
-                                                                                                                                                                                                                                    d = fastnp.dot(v1,v2.T)
-                                                                                                                                                                                                                                        # is d greater than the threshold?
-                                                                                                                                                                                                                                            res = d> threshold
-                                                                                                                                                                                                                                                
-                                                                                                                                                                                                                                                    ### END CODE HERE ###
-                                                                                                                                                                                                                                                        
-                                                                                                                                                                                                                                                            if(verbose):
-                                                                                                                                                                                                                                                                    print("Q1  = ", Q1, "\nQ2  = ", Q2)
-                                                                                                                                                                                                                                                                            print("d   = ", d)
-                                                                                                                                                                                                                                                                                    print("res = ", res)
+    Returns:
+        bool: True if the questions are duplicates, False otherwise.
+    """
+    ### START CODE HERE (Replace instances of 'None' with your code) ###
+    # use `nltk` word tokenize function to tokenize
+    q1 = nltk.word_tokenize(question1)  # tokenize
+    q2 = nltk.word_tokenize(question2)  # tokenize
+    Q1, Q2 = [], [] # @KEEPTHIS
+    for word in q1:  # encode q1
+        # increment by checking the 'word' index in `vocab`
+        Q1 += [vocab[word]]
+        # GRADING COMMENT: Also valid to use
+        # Q1.extend([vocab[word]])
+    for word in q2:  # encode q2
+        # increment by checking the 'word' index in `vocab`
+        Q2 +=  [vocab[word]]
+        # GRADING COMMENT: Also valid to use
+        # Q2.extend([vocab[word]])
+    
+    # GRADING COMMENT: Q1 and Q2 need to be nested inside another list
+    # Call the data generator (built in Ex 01) using next()
+    # pass [Q1] & [Q2] as Q1 & Q2 arguments of the data generator. Set batch size as 1
+    # Hint: use `vocab['<PAD>']` for the `pad` argument of the data generator
+    Q1, Q2 = next(data_generator([Q1], [Q2], 1, pad=vocab['<PAD>'], shuffle=False))
+    # Call the model
+    v1, v2 = model((Q1,Q2))
+    # take dot product to compute cos similarity of each pair of entries, v1, v2
+    # don't forget to transpose the second argument
+    d = fastnp.dot(v1,v2.T)
+    # is d greater than the threshold?
+    res = d> threshold
+    
+    ### END CODE HERE ###
+    
+    if(verbose):
+        print("Q1  = ", Q1, "\nQ2  = ", Q2)
+        print("d   = ", d)
+        print("res = ", res)
 
-                                                                                                                                                                                                                                                                                        return res
+    return res                                                                                                                                                                                                                                                                           return res
 # 1 means it is duplicated, 0 otherwise
 predict(question1 , question2, 0.7, model, vocab, verbose = True)
 
